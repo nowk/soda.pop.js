d2a332c13a866e5c04f1f002970f30434d5815f6
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2013-11-18 05:07:17 -0500

    step function to add steps

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ Soda.Pop = {steps: {}};
 Soda.Pop.for = function(key, steps) {
   for(var step in steps) {
     if (steps.hasOwnProperty(step)) {
-      this.steps[step] = steps[step];
+      Soda.Pop.step(step, steps[step]);
     }
   }
 };
@@ -72,8 +72,28 @@ Soda.Pop.for = function(key, steps) {
  */
 
 Soda.Pop.use = function(filename) {
-  var steps = require(__dirname+'/../steps/'+filename);
-  Soda.Pop.for(steps[0], steps[1]);
+  var steps = require(__dirname+'/test/steps/'+filename);
+
+  if (steps && steps.length === 2) {
+    Soda.Pop.for(steps[0], steps[1]);
+  }
+};
+
+
+/*
+ * add indvidual step
+ *
+ *    step('I login with password', function(password) {
+ *      this
+ *        .type('login[password]', password)
+ *        .typeKeys('login[password]', password)
+ *    });
+ *
+ * @api public
+ */
+
+Soda.Pop.step = function(stepname, fn) {
+  Soda.Pop.steps[stepname] = fn;
 };
 
 
